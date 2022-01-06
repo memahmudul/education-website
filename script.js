a0fca42b3fd8ec21a49a32eb7f5f404195fe6d6a
memahmudul <farcrymahmudul@gmail.com>
2022-01-06 12:40:55 +0600

    home sections slider created

--- script.js
+++ script.js
@@ -39,3 +39,12 @@ loginBtn.onclick = ()=>{
     loginForm.classList.add('active')
     registerForm.classList.remove('active')
 }
+
+var swiper = new Swiper(".home-slider", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    loop : true,
+    grabCursor : true,
+  });
